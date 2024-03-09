12e8fa159e39769c0de7eaa33b84341f279cf559
James Sorensen <james@itsallelectric.org>
2024-03-08 23:25:18 -0700

    dudette

--- administrator/administrator.js
+++ administrator/administrator.js
@@ -38,7 +38,7 @@ const mqttClientId = `${clientId}_${adminId}_${generator().toString(16).slice(10
 global.aaa = {
   startTime: Date.now(),
   status: {
-    debugLevel: 1,
+    debugLevel: 0,
     mqttConnected: 0,
     mqttSubscribed: 0,
     mqttUnsubscribed: 0,
